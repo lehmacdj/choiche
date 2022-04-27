af823e50f9241cf96bb373408ce4bbb48c9df0f3
Devin Lehmacher <lehmacdj@gmail.com>
2022-04-26 19:38:58 -0700

    Initial Commit

---
+++ Choose/ChooseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChooseApp.swift
+//  Choose
+//
+//  Created by Devin Lehmacher on 4/26/22.
+//
+
+import SwiftUI
+
+@main
+struct ChooseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
